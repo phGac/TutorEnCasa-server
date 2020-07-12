6dd4a5dc38a230d1ba8e96d33d1cbebc629f6545
phgac <baladaphilippe@gmail.com>
2020-07-12 00:42:28 -0400

    Actualizaciones:     - arreglo error al confirmar pago

--- src/routes/payment.route.ts
+++ src/routes/payment.route.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { isNotLoggedIn } from '../middlewares/session.middleware';
+import { PaymentValidatorController, PaymentController } from '../controllers/payment.controller';
 
 const router = Router();
 
-router.get('/confirm', isNotLoggedIn, );
+router.post('/confirm', isNotLoggedIn, PaymentValidatorController.confirm, PaymentController.confirm);
 
 export default router;
